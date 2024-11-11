1758ae8d072b1d0d506c666b5a3b07583f4ca980
Redul94 <redul15-3193@diu.edu.bd>
2024-11-11 11:45:11 +0600

    update title

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,8 +13,8 @@ const Navbar = () => {
   return (
     <div className="bg-red-950 px-6 fixed top-0 left-0 w-full z-50">
       <div className="flex items-center justify-around w-full">
-        <div className="flex items-center gap-4">
-          <Image src="/logo.jpg" alt="Restaurant Logo" width={40} height={40} />
+        <div className="flex items-center">
+          <Image src="/logom.png" alt="Restaurant Logo" width={20} height={20} />
           <a className="btn btn-ghost text-xl">Restaurant</a>
         </div>
 
